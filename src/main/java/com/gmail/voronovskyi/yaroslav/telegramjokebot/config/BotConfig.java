e21e79fd95559446982bf0f11b3235a2edbd5c3a
yaroslav <yaroslav.voronovskyi@gmail.com>
2023-06-06 15:57:41 +0300

    Created configuration for app.

---
+++ src/main/java/com/gmail/voronovskyi/yaroslav/telegramjokebot/config/BotConfig.java
@@ -0,0 +1,18 @@
+package com.gmail.voronovskyi.yaroslav.telegramjokebot.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Data
+@Configuration
+@PropertySource("classpath:applications.properties")
+public class BotConfig {
+
+    @Value("${bot.name}")
+    private String botUserName;
+
+    @Value("${bot.token}")
+    private String token;
+}
---
+++ src/main/java/com/gmail/voronovskyi/yaroslav/telegramjokebot/config/BotInitializer.java
@@ -0,0 +1,31 @@
+package com.gmail.voronovskyi.yaroslav.telegramjokebot.config;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
+import org.telegram.telegrambots.meta.generics.TelegramBot;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class BotInitializer {
+
+    private TelegramBot bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot((LongPollingBot) bot);
+        } catch (TelegramApiRequestException exception) {
+            log.error(exception.getMessage());
+        }
+    }
+}
